31055d45424083f072883eb2810bf84309b68834
neaguIF21 <neagu.ionut0412@gmail.com>
2022-01-18 16:23:44 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("hello there")
+
+//animation script 
+var typed = new Typed(".typing",{
+    strings:["design","web development","travel","create content","photograhy"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
+
+let submitBtn = document.querySelector(".submit-button");
+
+submitBtn.addEventListener("click",function(e){
+    console.log("Clicked")
+    e.preventDefault();
+    let before=document.querySelector(".before-submit");
+    console.log(before)
+    
+    before.style.display="none"
+    let newH4 = document.createElement("h4");
+    let newP = document.createElement("p");
+    var textH4 = document.createTextNode("Something went wrong");         
+    newH4.appendChild(textH4); 
+    var textP = document.createTextNode("You can still contact me via email, phone number, social media");         
+    newP.appendChild(textP); 
+
+    let newElement = document.querySelector("#submit-change")
+    newElement.appendChild(newH4)
+    newElement.appendChild(newP)
+
+})
+
