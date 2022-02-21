c8ba4fc2116aa9acfd2fe5bd59aebc7bbff1a9e0
neaguIF21 <neagu.ionut0412@gmail.com>
2022-02-21 21:51:05 +0200

    calculator kg

--- index.js
+++ index.js
@@ -30,3 +30,31 @@ submitBtn.addEventListener("click",function(e){
 
 })
 
+
+//calculator
+
+let btnUserCalculat = document.querySelector("#userCalculat");
+
+
+btnUserCalculat.addEventListener("click",function(e){
+    // let userKG = parseInt(document.querySelector("#userKG").value);
+    let userInaltime = parseInt(document.querySelector("#userInaltime").value);
+    let userVarsta = parseInt(document.querySelector("#userVarsta").value);
+    let userSex = parseInt(document.querySelector("#userSex").value);
+
+    let showCalcul = document.querySelector("#afterCalcul");
+    console.log( userSex)
+    console.log("Clicked")
+    e.preventDefault();
+    let formula = 0
+    if (userSex == "1") {
+        formula = userInaltime - 100 - [ ((userInaltime - 150)/4) + ((userVarsta-20)/4)]
+    } else 
+    {
+        formula = userInaltime - 100 - [ ((userInaltime - 150)/2.5) + ((userVarsta-20)/6)]
+    }
+    showCalcul.innerHTML = formula
+
+    
+
+})
