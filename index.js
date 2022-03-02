e350268c6fcb1cd08e1b5a69d9d6cdc9ef1e54e3
neaguIF21 <neagu.ionut0412@gmail.com>
2022-03-02 21:03:26 +0200

    navbar position

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("hello there")
 
 //animation typing script 
 var typed = new Typed(".typing",{
-    strings:["design","fotografie","calatorie","creare continut","stil de viata sanatos"],
+    strings:["design","fotografie","calatorie","social media","nutritie"],
     typeSpeed:100,
     backSpeed:60,
     loop:true,
@@ -57,30 +57,3 @@ submitBtn.addEventListener("click",function(e){
 })
 
 
-//calculator
-
-let btnUserCalculat = document.querySelector("#userCalculat");
-
-
-btnUserCalculat.addEventListener("click",function(e){
-    // let userKG = parseInt(document.querySelector("#userKG").value);
-    let userInaltime = parseInt(document.querySelector("#userInaltime").value);
-    let userVarsta = parseInt(document.querySelector("#userVarsta").value);
-    let userSex = parseInt(document.querySelector("#userSex").value);
-
-    let showCalcul = document.querySelector("#afterCalcul");
-    console.log( userSex)
-    console.log("Clicked")
-    e.preventDefault();
-    let formula = 0
-    if (userSex == "1") {
-        formula = userInaltime - 100 - [ ((userInaltime - 150)/4) + ((userVarsta-20)/4)]
-    } else 
-    {
-        formula = userInaltime - 100 - [ ((userInaltime - 150)/2.5) + ((userVarsta-20)/6)]
-    }
-    showCalcul.innerHTML = formula
-
-    
-
-})
