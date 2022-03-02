ca7a867c5bcc5ff5d490d1a61879b38c17cabd9d
neaguIF21 <neagu.ionut0412@gmail.com>
2022-03-02 18:09:21 +0200

    text and navbar added

--- index.js
+++ index.js
@@ -8,6 +8,32 @@ var typed = new Typed(".typing",{
     loop:true,
 })
 
+//navigation
+
+let toogleButton = document.querySelector("#nav-icon4")
+let navbarLinks = document.querySelector(".navbar-links")
+
+toogleButton.addEventListener("click",()=>{
+    navbarLinks.classList.toggle('active')
+    toogleButton.classList.toggle('open')
+})
+
+navbarLinks.addEventListener("click",()=>{
+  navbarLinks.classList.toggle('active')
+  toogleButton.classList.toggle('open')
+})
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 //contact form submit
 let submitBtn = document.querySelector(".submit-button");
 submitBtn.addEventListener("click",function(e){
